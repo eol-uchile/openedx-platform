91718bb8608728bd2039ba9d236fac5b570e9826
Felipe Espinoza <felipe.espinoza.r@uchile.cl>
2020-04-14 21:54:48 +0000

    Adding unit test for uchileedxlogin modifications

---
+++ .github/test/tests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd /openedx/edx-platform
+
+# Test EOL Modifications
+
+# Task Helper modifications for uchileedxlogin package
+## Test before installing the package
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest lms/djangoapps/instructor_task/tests/test_tasks_helper.py lms/djangoapps/instructor_analytics/tests/test_basic.py
+
+## Test after installing the package
+pip install -e git+https://github.com/eol-uchile/uchileedxlogin@2db9adab2a3662ae04754cc158af57f2f197c236#egg=uchileedxlogin
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest lms/djangoapps/instructor_task/tests/test_tasks_helper.py lms/djangoapps/instructor_analytics/tests/test_basic.py
+
