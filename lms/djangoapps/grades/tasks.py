226eb9f18ad6eec4099f8ed67c42e3430ee7c91c
Leangseu Kim <lkim@edx.org>
2021-08-17 10:47:33 -0400

    fix: grading from Staff Graded Assignment xblocks are not successfully being passed
    block_type isn't guarantee to be the same as item_type. In this case, item_id should be enough for filtering.
    
    Ticket [AU-70]
    
    (cherry picked from commit 6985e1388b4af5f2dd06e3f372822a0abf249320)

--- lms/djangoapps/grades/tasks.py
+++ lms/djangoapps/grades/tasks.py
@@ -263,9 +263,8 @@ def _has_db_updated_with_new_score(self, scored_block_usage_key, **kwargs):
         score = sub_api.get_score(
             {
                 "student_id": kwargs['anonymous_user_id'],
-                "course_id": six.text_type(scored_block_usage_key.course_key),
-                "item_id": six.text_type(scored_block_usage_key),
-                "item_type": scored_block_usage_key.block_type,
+                "course_id": str(scored_block_usage_key.course_key),
+                "item_id": str(scored_block_usage_key),
             }
         )
         found_modified_time = score['created_at'] if score is not None else None
