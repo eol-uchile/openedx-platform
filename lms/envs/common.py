4a9c3d8320510d2046aece5cd0bba02485c45565
fsologureng <fsologur@uchile.cl>
2024-01-03 10:36:49 -0300

    Add required environment variables to LMS Cherry pick `lms/envs/common.py` file from cfaea69305bbea99e1fb3884ed67bd3f2c51c931

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -4202,3 +4202,9 @@ LOGO_URL_PNG = None
 LOGO_TRADEMARK_URL = None
 FAVICON_URL = None
 DEFAULT_EMAIL_LOGO_URL = 'https://edx-cdn.org/v3/default/logo.png'
+
+############## EOL #########################
+REGISTRATION_EMAIL_PATTERNS_DISALLOWED = None
+
+############## Grade Report - uchileedxlogin #########################
+UCHILEEDXLOGIN_TASK_RUN_ENABLE = False
