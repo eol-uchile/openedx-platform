7f0d4e7f0b2f093206a03b690c2272a35209cd03
fsologureng <fsologur@uchile.cl>
2026-08-12 10:06:00 -0400

    refactor(eol/koa): rm boto2 unused imports video upload

--- cms/djangoapps/contentstore/views/videos.py
+++ cms/djangoapps/contentstore/views/videos.py
@@ -14,8 +14,6 @@ from uuid import uuid4
 
 import six
 import boto3
-from boto import s3
-from boto.sts import STSConnection
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.staticfiles.storage import staticfiles_storage
