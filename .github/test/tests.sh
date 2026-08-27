64a27f28f6cf81e349f11a5b5ed9271f19d16536
fsologureng <fsologur@uchile.cl>
2026-08-27 12:35:29 -0400

    ci(ou): add tests for latest cherry-picks
    (cherry picked from commit 173333ce2d7aee19f7ceed1ca16b32751341ac4c)

--- .github/test/tests.sh
+++ .github/test/tests.sh
@@ -8,8 +8,15 @@ mkdir -p test_root/log/
 
 # Test EOL Modifications
 DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest --exitfirst --full-trace --verbose \
-	lms/djangoapps/instructor_task/tests/test_tasks_helper.py \
-	lms/djangoapps/instructor_analytics/tests/test_basic.py \
-	lms/djangoapps/certificates/tests/test_webview_views.py \
-	lms/djangoapps/bulk_email/tests/ \
-	lms/djangoapps/instructor/tests/
+    openedx/features/course_experience/tests/views/test_course_home.py \
+    lms/djangoapps/courseware/tests/test_courses.py \
+    lms/djangoapps/instructor_task/tests/test_tasks_helper.py \
+    lms/djangoapps/instructor_analytics/tests/test_basic.py \
+    lms/djangoapps/certificates/tests/test_webview_views.py \
+    lms/djangoapps/bulk_email/tests/ \
+    lms/djangoapps/instructor/tests/ \
+    common/djangoapps/util/tests/test_file.py \
+    common/djangoapps/util/tests/test_sandboxing.py \
+    lms/djangoapps/courseware/tests/test_module_render.py \
+    lms/djangoapps/lti_provider/tests/test_signature_validator.py \
+    lms/djangoapps/staticbook/tests.py
